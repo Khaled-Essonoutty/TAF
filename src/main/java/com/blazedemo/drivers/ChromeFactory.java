a9851b2370162631eb3b7747ccbc631ba2524961
Khaled Essonoutty <k.essonoutty74@gmail.com>
2025-11-10 14:07:25 +0200

    Fix: Fixing itemAddedLabel Locator

--- src/main/java/com/blazedemo/drivers/ChromeFactory.java
+++ src/main/java/com/blazedemo/drivers/ChromeFactory.java
@@ -48,7 +48,7 @@ public class ChromeFactory extends AbstractDriver {
         options.addArguments("--start-maximized");
         Map<String, Object> prefs = new HashMap<>();
         String userDir = System.getProperty("user.dir");
-        String downloadPath = userDir + "\\src\\test\\resources\\downloads";
+        String downloadPath = userDir + "/src/test/resources/downloads";
         prefs.put("profile.default_content_settings.popups", 0);
         prefs.put("download.prompt_for_download", false);
         prefs.put("download.default_directory", downloadPath);
